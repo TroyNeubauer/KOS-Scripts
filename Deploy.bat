7445f351621f8a1933f33dc5bf37fa33f9c718ec
Troy Neubauer <troyneubauer@gmail.com>
2018-04-17 12:49:44 -0400

    Added Launcher 2.0 craft file

--- Deploy.bat
+++ Deploy.bat
@@ -2,5 +2,6 @@ xcopy "C:\KSP" "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Progr
 xcopy "C:\KSP" "C:\Modded ksp\Ships\Script" /e /i /y
 
 xcopy "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\saves\1_2_2 Modded Sandbox\Ships\VAB\Fast Ting.craft" "C:\KSP" /e /i /y
+xcopy "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\saves\1_2_2 Modded Sandbox\Ships\VAB\Fast Ting 2_0.craft" "C:\KSP" /e /i /y
 
 PAUSE
