bd6b9110357e7465342236e51702f14ed11f6932
Troy Neubauer <troyneubauer@gmail.com>
2018-06-27 07:20:19 -0400

    Fixed paths in deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -1,5 +1,2 @@
-xcopy "C:\KSP" "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\Ships\Script" /e /i /y
-xcopy "C:\KSP" "C:\Modded ksp\Ships\Script" /e /i /y
-
-xcopy "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\saves\1_2_2 Modded Sandbox\Ships\VAB\Fast Ting.craft" "C:\KSP" /e /i /y
-xcopy "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\saves\1_2_2 Modded Sandbox\Ships\VAB\Fast Ting 2_0.craft" "C:\KSP" /e /i /y
+xcopy "C:\KSP\Programs" "C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program\Ships\Script" /e /i /y
+xcopy "C:\KSP\Programs" "C:\KSP\KSP - 1.4\Ships\Script" /e /i /y
